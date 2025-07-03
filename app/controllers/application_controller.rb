2fa7eb6ab16b4a0be3fd8a44e785334ada6a78b2
tsuruya82 <himawaritabuk@gmail.com>
2025-07-03 15:18:22 +0900

    アカウントページの詳細・編集機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-  private
-
-  def after_sign_in_path_for(resource_or_scope)
-    edit_user_registration_path
-  end
-
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/top'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-
+  get "users/show", to: "users#show"
   root to: 'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
