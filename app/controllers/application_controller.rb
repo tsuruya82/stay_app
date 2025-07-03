b1793b561f58a162136da6dddccd4ae373bf7736
tsuruya82 <himawaritabuk@gmail.com>
2025-07-03 11:39:05 +0900

    ログイン・ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def after_sign_in_path_for(resource_or_scope)
+    edit_user_registration_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
