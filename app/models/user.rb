3bec3d34651797682ea7d57265712dd2ba9ded13
tsuruya82 <himawaritabuk@gmail.com>
2025-07-04 11:30:49 +0900

    ユーザー関連の実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
   mount_uploader :icon_image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2025_06_23_151110) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name"
+    t.string "name", null: false
     t.string "icon_image"
     t.string "introduce"
     t.string "reset_password_token"
